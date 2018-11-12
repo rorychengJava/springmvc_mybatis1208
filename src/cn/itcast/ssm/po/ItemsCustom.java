7a2464929132c5b5e372a1aa115e99401a492edd
rorychengJava <rorycheng@yeah.net>
2018-11-12 22:16:53 +0800

    springmvc更新

--- src/cn/itcast/ssm/po/ItemsCustom.java
+++ src/cn/itcast/ssm/po/ItemsCustom.java
@@ -12,5 +12,5 @@ package cn.itcast.ssm.po;
 public class ItemsCustom extends Items {
 	
 	//添加商品信息的扩展属性
-
+	
 }
--- src/cn/itcast/ssm/po/ItemsQueryVo.java
+++ src/cn/itcast/ssm/po/ItemsQueryVo.java
@@ -1,5 +1,7 @@
 package cn.itcast.ssm.po;
 
+import java.util.List;
+
 /**
  * 
  * <p>Title: ItemsQueryVo</p>
@@ -16,6 +18,12 @@ public class ItemsQueryVo {
 	
 	//为了系统 可扩展性，对原始生成的po进行扩展
 	private ItemsCustom itemsCustom;
+	
+	//批量商品信息
+	private List<ItemsCustom> itemsList;
+	
+	//用户信息
+	//private UserCustom userCustom;
 
 	public Items getItems() {
 		return items;
@@ -31,6 +39,14 @@ public class ItemsQueryVo {
 
 	public void setItemsCustom(ItemsCustom itemsCustom) {
 		this.itemsCustom = itemsCustom;
+	}
+
+	public List<ItemsCustom> getItemsList() {
+		return itemsList;
+	}
+
+	public void setItemsList(List<ItemsCustom> itemsList) {
+		this.itemsList = itemsList;
 	}
 	
 	
